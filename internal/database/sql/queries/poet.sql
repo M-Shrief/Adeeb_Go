792477a19b232d1568b0aba20cb45aa0cbcc86ca
m-shrief <m.shrief.dev@proton.me>
2024-10-11 23:50:40 +0300

    Limited returning rows from Poet's queries.

--- internal/database/sql/queries/poet.sql
+++ internal/database/sql/queries/poet.sql
@@ -1,21 +1,20 @@
 
 -- name: CreatePoet :one
-INSERT INTO poets (name, bio, time_period) VALUES ($1, $2, $3) RETURNING *;
--- like INSERT INTO users (name, password, roles) VALUES ('nameasf', 'sfaasffas', ARRAY['DBA']::role[]) RETURNING *;
+INSERT INTO poets (name, bio, time_period) VALUES ($1, $2, $3) RETURNING id,name,bio,time_period;
 
 -- name: GetPoets :many
-SELECT * FROM poets;
+SELECT id,name,bio,time_period FROM poets;
 
 -- name: GetPoetById :one
-SELECT * FROM poets WHERE id = $1 LIMIT 1;
+SELECT id,name,bio,time_period FROM poets WHERE id = $1 LIMIT 1;
 
 -- -- name: UpdatePoet :exec
--- UPDATE users SET
+-- UPDATE poets SET
 --   name = COALESCE(NULLIF($2::varchar, ''), name),
 --   password = COALESCE(NULLIF($3::varchar, ''), password),
 --   roles = COALESCE(NULLIF($4::role[], ARRAY[]::role[]), roles),
 --   update_at = CURRENT_TIMESTAMP
 -- WHERE id = $1;
 
--- -- name: DeleteUser :exec
--- DELETE FROM users WHERE id = $1;
+-- -- name: DeletePoet :exec
+-- DELETE FROM poets WHERE id = $1;
