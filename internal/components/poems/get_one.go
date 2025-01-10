30d5f5c909a7350d8961b0e8c45b4068af4c492e
m-shrief <m.shrief.dev@proton.me>
2025-01-10 10:36:40 +0200

    Added Poems components GET /poems/:id route

---
+++ internal/components/poems/get_one.go
@@ -0,0 +1,37 @@
+package poems
+
+import (
+	"Adeeb_Go/internal/database/sqlc"
+	"context"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+)
+
+type GetOnePoemInput struct {
+	ID string `path:"id" maxLength:"100" doc:"Poem's ID"`
+}
+
+type GetOnePoemOutput struct {
+	Body   Poem
+	Status int
+}
+
+func GetOnePoemHandler(ctx context.Context, input *GetOnePoemInput) (*GetOnePoemOutput, error) {
+	db := sqlc.GetDBTX()
+
+	row := db.QueryRow(ctx, "SELECT id,intro,poet_id,verses FROM poems WHERE id = $1;", input.ID)
+
+	var poem Poem
+	err := row.Scan(&poem.Id, &poem.Intro, &poem.Poet_id, &poem.Verses)
+	if err != nil {
+		return nil, huma.Error404NotFound("Poem's not found")
+	}
+
+	resp := &GetOnePoemOutput{
+		Body:   poem,
+		Status: http.StatusOK,
+	}
+
+	return resp, nil
+}
